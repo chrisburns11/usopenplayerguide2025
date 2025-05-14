f788a86e2ff204a5c3472814a97550c4eaeaf050
chrisburns11 <ChristopherBurns11@Gmail.com>
2025-05-14 15:05:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
     section = request.args.get("section", "Welcome Message")
-    return render_template("index.html", section=section)
+    dark_mode = request.cookies.get("darkMode", "false")
+    resp = make_response(render_template("index.html", section=section, dark_mode=dark_mode))
+    return resp
 
 if __name__ == "__main__":
     app.run(debug=False, port=5001)
